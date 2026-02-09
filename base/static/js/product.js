1d7d1dc893e5e135cb5378c29fd624154895e902
Owen Dechow <owen.dechow@gmail.com>
2026-02-08 21:14:40 -0500

    more wording changes

--- base/static/js/product.js
+++ base/static/js/product.js
@@ -1,22 +1,18 @@
 productId = undefined;
 
-function sendProductViewReport() {
-    fetch(`/product-view/${encodeURIComponent(getuid())}/${encodeURIComponent(productId)}`);
-}
-
 function toggleDonation(event) {
-    let dontationSet = event.target.closest(".donation-set");
-    let input = dontationSet.querySelector("input[name=donation]");
+    let donationSet = event.target.closest(".donation-set");
+    let input = donationSet.querySelector("input[name=donation]");
 
     input.value = "";
     if (event.target.checked) {
-        dontationSet.classList.remove("off");
-        dontationSet.classList.add("on");
+        donationSet.classList.remove("off");
+        donationSet.classList.add("on");
         input.removeAttribute("disabled");
     } else {
         input.setAttribute("disabled", "true");
-        dontationSet.classList.remove("on");
-        dontationSet.classList.add("off");
+        donationSet.classList.remove("on");
+        donationSet.classList.add("off");
     }
 }
 
@@ -25,25 +21,25 @@ function clearParent(event) {
 }
 
 function selectVariation(event) {
-    let vId = event.target.getAttribute("variation")
-    let inStock = event.target.getAttribute("in-stock")
+    let vId = event.target.getAttribute("variation");
+    let inStock = event.target.getAttribute("in-stock");
 
-    let selectedBtn = document.querySelector(".variation-tab-btn.selected")
-    if (selectedBtn) selectedBtn.classList.remove("selected")
+    let selectedBtn = document.querySelector(".variation-tab-btn.selected");
+    if (selectedBtn) selectedBtn.classList.remove("selected");
 
-    let selectedTab = document.querySelector(".variation.selected")
-    if (selectedTab) selectedTab.classList.remove("selected")
+    let selectedTab = document.querySelector(".variation.selected");
+    if (selectedTab) selectedTab.classList.remove("selected");
 
-    document.querySelector(`.variation[variation="${vId}"]`).classList.add("selected")
-    document.querySelector(`.variation-tab-btn[variation="${vId}"]`).classList.add("selected")
+    document.querySelector(`.variation[variation="${vId}"]`).classList.add("selected");
+    document.querySelector(`.variation-tab-btn[variation="${vId}"]`).classList.add("selected");
 
-    let form = document.querySelector("#order-form")
+    let form = document.querySelector("#order-form");
     if (form) {
         if (parseInt(inStock) > 0) {
-            form.style.display = ""
-            form.querySelector("[name=product]").value = vId
+            form.style.display = "";
+            form.querySelector("[name=product]").value = vId;
         } else {
-            form.style.display = "none"
+            form.style.display = "none";
         }
     }
 }
